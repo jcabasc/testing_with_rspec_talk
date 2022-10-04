379b485b8b3ee59a884febd8933ccb0aff0e9dfd
Jonathan Cabas C <jonathan.cabas@skydropx.com>
2022-10-04 11:16:25 -0500

    Sets up test environment

---
+++ spec/factories/carriers.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :carrier do
+    name { 'Test' }
+    code { 'test' }
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+load 'ar.rb'
+require 'rspec'
+require 'factory_bot'
+load 'support/factory_bot.rb'
